b98c7b933267563ed31d4683b40cb7d8faeaee1d
Attacktive <welcome.hell.dude@gmail.com>
2022-05-24 14:38:26 +0900

    Rename variables

--- kvs_archive.py
+++ kvs_archive.py
@@ -4,20 +4,21 @@ import argparse
 parser = argparse.ArgumentParser(description="AOT2 .ktsl2stbin files archiving tool.")
 parser.add_argument("folder", help="folder with .kvs files")
 args = parser.parse_args()
-kfolder = args.folder
+k_folder = args.folder
 
 
 def read_bytes(filename):
     print("Reading file " + filename)
     b_list = []
 
-    f = open(filename, "rb")
+    file_stream = open(filename, "rb")
     while True:
-        piece = f.read(1024)
+        piece = file_stream.read(1024)
         if not piece:
             break
+
         b_list.append(piece)
-    f.close()
+    file_stream.close()
 
     return b_list
 
@@ -28,15 +29,15 @@ def write_file(name):
     # This probably only works with AOT2 because the header might be different in each game
     header = b"KTSR\x02\x94\xdd\xfc\x01\x00\x00\x016\x0e\xf4\x05\x00\x00\x00\x00\x00\x00\x00\x00\x10\xf7\x05&\x10\xf7\x05&\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\t\xd4\xf4\x15 \xe9\x88\x00\xca\xab\xa8\xa9 \x00\x00\x00\xf7\xe8\x88\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 
-    new_file = open(name, "wb")
-    new_file.write(header + b"".join(fbytes))
-    new_file.close()
+    new_file_stream = open(name, "wb")
+    new_file_stream.write(header + b"".join(file_bytes))
+    new_file_stream.close()
 
 
-files = glob.glob(kfolder + "/*.[kK][vV][sS]")
-fbytes = []
+files = glob.glob(k_folder + "/*.[kK][vV][sS]")
+file_bytes = []
 
-for f in files:
-    fbytes.append(b"".join(read_bytes(f)))
+for file in files:
+    file_bytes.append(b"".join(read_bytes(file)))
 
 write_file("mod.ktsl2stbin")
--- kvs_extract.py
+++ kvs_extract.py
@@ -7,29 +7,29 @@ import argparse
 parser = argparse.ArgumentParser(description=".ktsl2stbin files extraction tool.")
 parser.add_argument("file", help=".ktsl2stbin file")
 args = parser.parse_args()
-kfile = args.file
+k_file = args.file
 
 
 def read_bytes(filename):
     print("Reading file...")
     b_list = []
 
-    f = open(filename, "rb")
+    file_stream = open(filename, "rb")
     while True:
-        piece = f.read(1024)
+        piece = file_stream.read(1024)
         if not piece:
             break
         b_list.append(piece)
-    f.close()
+    file_stream.close()
 
     return b_list
 
 
 def write_file(name, start, end):
     print("Writing file " + name)
-    new_file = open(name, "wb")
-    new_file.write(byte_str[start:end])
-    new_file.close()
+    new_file_stream = open(name, "wb")
+    new_file_stream.write(byte_str[start:end])
+    new_file_stream.close()
 
 
 def find_all(a_str, sub):
@@ -43,7 +43,7 @@ def find_all(a_str, sub):
         start += len(sub)
 
 
-byte_str = b"".join(read_bytes(kfile))
+byte_str = b"".join(read_bytes(k_file))
 
 ext = ".kvs"
 files_start = list(find_all(byte_str, "KOVS"))
@@ -52,15 +52,17 @@ if not files_start:
     files_start = list(find_all(byte_str, "KTSS"))
     ext = ".kns"
 
-last_offset = os.path.getsize(kfile) - 1
+last_offset = os.path.getsize(k_file) - 1
 
-out = os.path.join(os.path.dirname(kfile), os.path.basename(kfile).split(".")[0])
-os.makedirs(out, exist_ok=True)
+out_directory_name = os.path.join(os.path.dirname(k_file), os.path.basename(k_file).split(".")[0])
+os.makedirs(out_directory_name, exist_ok=True)
 
 size = len(files_start)
 print(size, "files")
 
 for i in range(size - 1):
-    write_file(os.path.join(out, (str(i) if i >= 10 else "0" + str(i)) + ext), files_start[i], files_start[i + 1])
+    out_file_name = os.path.join(out_directory_name, (str(i) if i >= 10 else "0" + str(i)) + ext)
+    write_file(out_file_name, files_start[i], files_start[i + 1])
 else:
-    write_file(os.path.join(out, str(size - 1) + ext), files_start[-1], last_offset)
+    out_file_name = os.path.join(out_directory_name, str(size - 1) + ext)
+    write_file(out_file_name, files_start[-1], last_offset)
